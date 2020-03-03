c9e89129001899d73dfa61f529cd4bd88341434b
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-03 20:02:45 +0700

    Configuration

---
+++ config/server.py
@@ -0,0 +1,10 @@
+update = {
+    'server.socket_host'        : "hostname"    ,
+    'server.socket_port'        : "port"        ,
+    'engine.autoreload.on'      : True          ,
+    'request.show_tracebacks'   : False         ,
+}
+
+# Public Variable
+FirstName   = "Your"
+LastName    = "name"
